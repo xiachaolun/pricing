4b6e42451d722ead59548674ceaef9c524526a7e
xiachaolun <sasnzy13@163.com>
2016-05-06 16:07:53 -0400

    1

--- src/data_generator.h
+++ src/data_generator.h
@@ -26,7 +26,7 @@ using namespace std;
 typedef tuple<int, int, int> Request;
 typedef vector<int> User;
 
-const int MAX_USER = 500000;
+const int MAX_USER = 1000000;
 const int MAX_BUYER = 1000;
 const int MAX_LABEL = 500;
 const int MAX_VALUATION = 1000;
